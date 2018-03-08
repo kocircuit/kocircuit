892384fd3ce2c8dfdba7602d9a2dff0ac3812d24
Petar Maymounkov <petarm@gmail.com>
2018-03-08 12:19:43 -0500

    eval/macros: add Require (variation on Expect which produces a compile-time error, rather than a runtime panic)

--- lang/go/eval/macros/expect.go
+++ lang/go/eval/macros/expect.go
@@ -10,6 +10,7 @@ import (
 
 func init() {
 	RegisterEvalMacro("Expect", EvalExpectMacro{})
+	RegisterEvalMacro("Require", EvalRequireMacro{})
 }
 
 type EvalExpectMacro struct{}
@@ -32,6 +33,25 @@ func (EvalExpectMacro) Invoke(span *Span, arg Arg) (returns Return, effect Effec
 				},
 			),
 		)
+	} else {
+		return a, nil, nil
+	}
+}
+
+type EvalRequireMacro struct{}
+
+func (m EvalRequireMacro) MacroID() string { return m.Help() }
+
+func (m EvalRequireMacro) Label() string { return "require" }
+
+func (m EvalRequireMacro) MacroSheathString() *string { return PtrString("Require") }
+
+func (m EvalRequireMacro) Help() string { return "Require" }
+
+func (EvalRequireMacro) Invoke(span *Span, arg Arg) (returns Return, effect Effect, err error) {
+	a := arg.(*StructSymbol).SelectMonadic()
+	if IsEmptySymbol(a) {
+		return nil, nil, span.Errorf(nil, "required value missing")
 	} else {
 		return a, nil, nil
 	}
