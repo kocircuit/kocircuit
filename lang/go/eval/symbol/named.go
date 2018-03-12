d1ff6be91c0cf8277c088f72ea4603721840c61a
Petar Maymounkov <petarm@gmail.com>
2018-03-12 12:44:51 -0400

    go/eval: add named symbol and type to carry named go types (and protos)

---
+++ lang/go/eval/symbol/named.go
@@ -0,0 +1,92 @@
+package symbol
+
+import (
+	"fmt"
+	"reflect"
+
+	. "github.com/kocircuit/kocircuit/lang/circuit/eval"
+	. "github.com/kocircuit/kocircuit/lang/circuit/model"
+	"github.com/kocircuit/kocircuit/lang/go/gate"
+	. "github.com/kocircuit/kocircuit/lang/go/kit/tree"
+)
+
+type NamedSymbol struct {
+	Value reflect.Value `ko:"name=value"`
+}
+
+func (named *NamedSymbol) String() string {
+	return Sprint(named.Value.Interface())
+}
+
+func (named *NamedSymbol) Equal(sym Symbol) bool {
+	if other, ok := sym.(*NamedSymbol); ok {
+		return reflect.DeepEqual(named.Value.Interface(), other.Value.Interface())
+	} else {
+		return false
+	}
+}
+
+func (named *NamedSymbol) Splay() Tree {
+	// XXX: Splay does not handle non-Ko fields (protobuf or generic go).
+	// Thus, hashing protocol buffers of native go types will have collisions.
+	return Splay(named.Value.Interface())
+}
+
+func (named *NamedSymbol) Hash() string {
+	return named.Splay().TreeHash()
+}
+
+func (named *NamedSymbol) Type() Type {
+	return &NamedType{
+		Type: reflect.TypeOf(named.Value),
+	}
+}
+
+func (named *NamedSymbol) Select(span *Span, path Path) (Shape, Effect, error) {
+	if len(path) == 0 {
+		return named, nil, nil
+	} else {
+		if step, err := named.Walk(span, path[0]); err != nil {
+			return nil, nil, err
+		} else {
+			return step.Select(span, path[1:])
+		}
+	}
+}
+
+func (named *NamedSymbol) Walk(span *Span, field string) (Symbol, error) {
+	v := named.Value
+	for v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	if !v.IsValid() {
+		return EmptySymbol{}, nil
+	}
+	if v.Kind() != reflect.Struct {
+		return nil, span.Errorf(nil, "cannot select %s into %v", field, named)
+	}
+	// XXX: gate.StripFields should recognize protobuf names
+	if fieldIndex, ok := gate.StripFields(v.Type()).FieldByKoName(field); ok {
+		return Deconstruct(span, v.Field(fieldIndex))
+	} else {
+		return EmptySymbol{}, nil
+	}
+}
+
+func (named *NamedSymbol) Augment(span *Span, _ Knot) (Shape, Effect, error) {
+	return nil, nil, span.Errorf(nil, "named value %v cannot be augmented", named)
+}
+
+func (named *NamedSymbol) Invoke(span *Span) (Shape, Effect, error) {
+	return nil, nil, span.Errorf(nil, "named value %v cannot be invoked", named)
+}
+
+type NamedType struct {
+	Type reflect.Type `ko:"name=type"`
+}
+
+func (named NamedType) IsType() {}
+
+func (named NamedType) Splay() Tree {
+	return NoQuote{fmt.Sprintf("%s.%s", named.Type.PkgPath(), named.Type.Name())}
+}
