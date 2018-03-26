33545e0575cb2e8ec5a4f8e0611ee8bb77a8f1c8
Petar Maymounkov <petarm@gmail.com>
2018-03-26 18:50:29 -0400

    ok

--- lang/go/eval/symbol/assemble.go
+++ lang/go/eval/symbol/assemble.go
@@ -15,17 +15,17 @@ func (ctx *typingCtx) Assemble(pbSymbol *pb.Symbol) Symbol {
 	if pbSymbol == nil {
 		return EmptySymbol{}
 	} else {
-		switch u := pbSymbol.Symbol.(type) {
+		switch pbSymbol.Symbol.(type) {
 		case *pb.Symbol_Basic:
-			return ctx.AssembleBasic(u.Basic)
+			return ctx.AssembleBasic(pbSymbol.GetBasic())
 		case *pb.Symbol_Series:
-			return ctx.AssembleSeries(u.Series)
+			return ctx.AssembleSeries(pbSymbol.GetSeries())
 		case *pb.Symbol_Struct:
-			return ctx.AssembleStruct(u.Struct)
+			return ctx.AssembleStruct(pbSymbol.GetStruct())
 		case *pb.Symbol_Map:
-			return ctx.AssembleMap(u.Map)
+			return ctx.AssembleMap(pbSymbol.GetMap())
 		case *pb.Symbol_Blob:
-			return ctx.AssembleBlob(u.Blob)
+			return ctx.AssembleBlob(pbSymbol.GetBlob())
 		default:
 			panic(ctx.Errorf(nil, "unknown symbol"))
 		}
@@ -36,31 +36,31 @@ func (ctx *typingCtx) AssembleBasic(pbBasic *pb.SymbolBasic) Symbol {
 	if pbBasic == nil {
 		return EmptySymbol{}
 	} else {
-		switch u := pbBasic.Basic.(type) {
+		switch pbBasic.Basic.(type) {
 		case *pb.SymbolBasic_Bool:
-			return BasicSymbol{Value: bool(u.Bool)}
+			return BasicSymbol{Value: bool(pbBasic.GetBool())}
 		case *pb.SymbolBasic_String_:
-			return BasicSymbol{Value: string(u.String_)}
+			return BasicSymbol{Value: string(pbBasic.GetString_())}
 		case *pb.SymbolBasic_Int8:
-			return BasicSymbol{Value: int8(u.Int8)}
+			return BasicSymbol{Value: int8(pbBasic.GetInt8())}
 		case *pb.SymbolBasic_Int16:
-			return BasicSymbol{Value: int16(u.Int16)}
+			return BasicSymbol{Value: int16(pbBasic.GetInt16())}
 		case *pb.SymbolBasic_Int32:
-			return BasicSymbol{Value: int32(u.Int32)}
+			return BasicSymbol{Value: int32(pbBasic.GetInt32())}
 		case *pb.SymbolBasic_Int64:
-			return BasicSymbol{Value: int64(u.Int64)}
+			return BasicSymbol{Value: int64(pbBasic.GetInt64())}
 		case *pb.SymbolBasic_Uint8:
-			return BasicSymbol{Value: uint8(u.Uint8)}
+			return BasicSymbol{Value: uint8(pbBasic.GetUint8())}
 		case *pb.SymbolBasic_Uint16:
-			return BasicSymbol{Value: uint16(u.Uint16)}
+			return BasicSymbol{Value: uint16(pbBasic.GetUint16())}
 		case *pb.SymbolBasic_Uint32:
-			return BasicSymbol{Value: uint32(u.Uint32)}
+			return BasicSymbol{Value: uint32(pbBasic.GetUint32())}
 		case *pb.SymbolBasic_Uint64:
-			return BasicSymbol{Value: uint64(u.Uint64)}
+			return BasicSymbol{Value: uint64(pbBasic.GetUint64())}
 		case *pb.SymbolBasic_Float32:
-			return BasicSymbol{Value: float32(u.Float32)}
+			return BasicSymbol{Value: float32(pbBasic.GetFloat32())}
 		case *pb.SymbolBasic_Float64:
-			return BasicSymbol{Value: float64(u.Float64)}
+			return BasicSymbol{Value: float64(pbBasic.GetFloat64())}
 		default:
 			panic(ctx.Errorf(nil, "unknown basic symbol"))
 		}
@@ -68,6 +68,9 @@ func (ctx *typingCtx) AssembleBasic(pbBasic *pb.SymbolBasic) Symbol {
 }
 
 func (ctx *typingCtx) AssembleSeries(pbSeries *pb.SymbolSeries) Symbol {
+	if pbSeries == nil {
+		return EmptySymbol{}
+	}
 	asmElems := make(Symbols, 0, len(pbSeries.Element))
 	ctx2 := ctx.Refine("()")
 	for _, elem := range pbSeries.Element {
@@ -83,7 +86,23 @@ func (ctx *typingCtx) AssembleSeries(pbSeries *pb.SymbolSeries) Symbol {
 }
 
 func (ctx *typingCtx) AssembleStruct(pbStruct *pb.SymbolStruct) Symbol {
-	panic("XXX")
+	if pbStruct == nil {
+		return EmptySymbol{}
+	}
+	asmFields := make(FieldSymbols, 0, len(pbStruct.Field))
+	for _, field := range pbStruct.Field {
+		ctx2 := ctx.Refine(field.GetName())
+		if asmFieldValue := ctx2.Assemble(field.GetValue()); !IsEmptySymbol(asmFieldValue) {
+			asmFields = append(asmFields,
+				&FieldSymbol{
+					Name:    field.GetName(),
+					Monadic: field.GetMonadic(),
+					Value:   asmFieldValue,
+				},
+			)
+		}
+	}
+	return MakeStructSymbol(asmFields)
 }
 
 func (ctx *typingCtx) AssembleMap(pbMap *pb.SymbolMap) Symbol {
