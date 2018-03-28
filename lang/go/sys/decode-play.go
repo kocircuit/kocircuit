e54e87127ac3762fe2e9edc32bd8bd17a159e88e
Petar Maymounkov <petarm@gmail.com>
2018-03-28 13:57:55 -0400

    go/sys: break up DecodePlay

--- lang/go/sys/decode-play.go
+++ lang/go/sys/decode-play.go
@@ -11,43 +11,53 @@ import (
 	"github.com/kocircuit/kocircuit/lang/go/runtime"
 )
 
-type DecodePlay struct {
-	RepoProtoBytes []byte  `ko:"name=repoProtoBytes"`
-	ArgProtoBytes  []byte  `ko:"name=argProtoBytes"`
-	Pkg            string  `ko:"name=pkg"`
-	Func           string  `ko:"name=func"`
-	Faculty        Faculty `ko:"name=faculty"`
+type Decode struct {
+	Repo    []byte  `ko:"name=repo"`
+	Arg     []byte  `ko:"name=arg"`
+	Pkg     string  `ko:"name=pkg"`
+	Func    string  `ko:"name=func"`
+	Faculty Faculty `ko:"name=faculty"`
 }
 
-func (arg *DecodePlay) Play(ctx *runtime.Context) *PlayResult {
-	repo, err := DecodeRepo(arg.RepoProtoBytes)
-	if err != nil {
-		panic(&DecodePlayError{Decode: err.Error()})
+type DecodeResult struct {
+	Faculty Faculty       `ko:"name=faculty"`
+	Repo    Repo          `ko:"name=repo"`
+	Func    *Func         `ko:"name=func"`
+	Eval    *Evaluate     `ko:"name=eval"`
+	Arg     *StructSymbol `ko:"name=arg"`
+}
+
+func (in *Decode) Play(ctx *runtime.Context) (result *DecodeResult, err error) {
+	result = &DecodeResult{}
+	if result.Repo, err = DecodeRepo(in.Repo); err != nil {
+		return nil, fmt.Errorf("decoding repo (%v)", err)
 	}
-	pfe := &PlayFuncEval{
-		Func: repo[arg.Pkg][arg.Func],
-		Eval: NewEvaluator(arg.Faculty, repo),
+	if result.Func = result.Repo[in.Pkg][in.Func]; result.Func == nil {
+		return nil, fmt.Errorf("cannot find function %s.%s", in.Pkg, in.Func)
 	}
-	// decode arg
-	argSym, err := DecodeSymbol(NewSpan(), pfe.Eval, arg.ArgProtoBytes)
-	if err != nil {
-		return &PlayResult{
-			Error: fmt.Errorf("decoding argument proto (%v)", err),
-		}
+	result.Eval = NewEvaluator(in.Faculty, result.Repo)
+	var sym Symbol
+	if sym, err = DecodeSymbol(NewSpan(), result.Eval, in.Arg); err != nil {
+		return nil, fmt.Errorf("decoding arg (%v)", err)
 	}
-	switch u := argSym.(type) {
+	switch u := sym.(type) {
 	case EmptySymbol:
+		result.Arg = nil
 	case *StructSymbol:
-		pfe.Arg = u
+		result.Arg = u
 	default:
-		return &PlayResult{
-			Error: fmt.Errorf("argument must be struct or empty, got %v", u),
-		}
+		return nil, fmt.Errorf("arg must be structure or empty, got %v", u)
 	}
-	// play
-	return pfe.Play(ctx)
+	return result, nil
 }
 
-type DecodePlayError struct {
-	Decode string `ko:"name=decode"`
+type DecodePlay DecodeResult
+
+func (in *DecodePlay) Play(ctx *runtime.Context) *PlayResult {
+	pfe := &PlayFuncEval{
+		Func: in.Func,
+		Eval: in.Eval,
+		Arg:  in.Arg,
+	}
+	return pfe.Play(ctx)
 }
