d97e01968bec9674d684a516608fa95468f53a41
Petar Maymounkov <petarm@gmail.com>
2018-04-24 16:48:36 -0400

    beginning of translate macro (for bootstrapping)

---
+++ lang/go/eval/translate/translate.go
@@ -0,0 +1,76 @@
+package translate
+
+import (
+	. "github.com/kocircuit/kocircuit/lang/circuit/eval"
+	. "github.com/kocircuit/kocircuit/lang/circuit/model"
+	. "github.com/kocircuit/kocircuit/lang/go/eval"
+	. "github.com/kocircuit/kocircuit/lang/go/eval/symbol"
+	. "github.com/kocircuit/kocircuit/lang/go/kit/util"
+)
+
+func init() {
+	RegisterEvalMacro("Translate", new(EvalTranslateMacro))
+}
+
+type EvalTranslateMacro struct{}
+
+func (m EvalTranslateMacro) MacroID() string { return m.Help() }
+
+func (m EvalTranslateMacro) Label() string { return "translate" }
+
+func (m EvalTranslateMacro) MacroSheathString() *string { return PtrString("Translate") }
+
+func (m EvalTranslateMacro) Help() string {
+	return "Translate(translator, func, ctx, arg)"
+}
+
+func (m EvalTranslateMacro) Doc() string {
+	return `XXX
+
+* translator is a structure of the form, where "->" indicates the return type of a variety:
+
+	(
+		Enter(step, ctx, arg) -> (returns, effect)
+		Leave(step, ctx, arg) -> (returns, effect)
+		Figure(step, ctx, literal) -> (returns, effect)
+		Select(step, ctx, arg) -> (returns, effect)
+		Augment(step, ctx, arg, knot) -> (returns, effect)
+		Invoke(step, ctx, arg) -> (returns, effect)
+		Combine(ctx, arg, stepResidues) -> (returns, effect)
+	)
+
+* ctx is a user object
+* effect is a user object
+* step
+* knot : XXX
+* literal : XXX
+* stepResidue is a sequence of step residue 4-tuples: (step, returned, effect, panicked)
+
+`
+}
+
+func (EvalTranslateMacro) Invoke(span *Span, arg Arg) (returns Return, effect Effect, err error) {
+	a := arg.(*StructSymbol)
+	// extract translator
+	translator, err := ExtractTranslator(span, a.Walk("translator"))
+	if err != nil {
+		return nil, nil, err
+	}
+	_ = translator
+	// extract function
+	fu, ok := a.Walk("func").(*VarietySymbol)
+	if !ok {
+		return nil, nil, span.Errorf(nil, "func must be a variety, got %v", a.Walk("func"))
+	}
+	interpretFunc, ok := fu.Macro.(*EvalInterpretMacro)
+	if !ok {
+		return nil, nil, span.Errorf(nil, "func must be a user function (not a builtin), got %v", fu.Macro)
+	}
+	_ = interpretFunc
+	// extract ctx and arg
+	ctxArg := a.Walk("ctx")
+	_ = ctxArg
+	argArg := a.Walk("arg")
+	_ = argArg
+	panic("o")
+}
---
+++ lang/go/eval/translate/translator.go
@@ -0,0 +1,46 @@
+package translate
+
+import (
+	. "github.com/kocircuit/kocircuit/lang/circuit/model"
+	. "github.com/kocircuit/kocircuit/lang/go/eval/symbol"
+)
+
+type Translator struct {
+	Enter   *VarietySymbol `ko:"name=Enter"`
+	Leave   *VarietySymbol `ko:"name=Leave"`
+	Figure  *VarietySymbol `ko:"name=Figure"`
+	Select  *VarietySymbol `ko:"name=Select"`
+	Augment *VarietySymbol `ko:"name=Augment"`
+	Invoke  *VarietySymbol `ko:"name=Invoke"`
+	Combine *VarietySymbol `ko:"name=Combine"`
+}
+
+func ExtractTranslator(span *Span, a Symbol) (*Translator, error) {
+	arg, ok := a.(*StructSymbol)
+	if !ok {
+		return nil, span.Errorf(nil, "translator must be a structure, got %v", a)
+	}
+	t := &Translator{}
+	if t.Enter, ok = arg.Walk("Enter").(*VarietySymbol); !ok {
+		return nil, span.Errorf(nil, "translator.Enter must be a variety, got %v", arg.Walk("Enter"))
+	}
+	if t.Leave, ok = arg.Walk("Leave").(*VarietySymbol); !ok {
+		return nil, span.Errorf(nil, "translator.Leave must be a variety, got %v", arg.Walk("Leave"))
+	}
+	if t.Figure, ok = arg.Walk("Figure").(*VarietySymbol); !ok {
+		return nil, span.Errorf(nil, "translator.Figure must be a variety, got %v", arg.Walk("Figure"))
+	}
+	if t.Select, ok = arg.Walk("Select").(*VarietySymbol); !ok {
+		return nil, span.Errorf(nil, "translator.Select must be a variety, got %v", arg.Walk("Select"))
+	}
+	if t.Augment, ok = arg.Walk("Augment").(*VarietySymbol); !ok {
+		return nil, span.Errorf(nil, "translator.Augment must be a variety, got %v", arg.Walk("Augment"))
+	}
+	if t.Invoke, ok = arg.Walk("Invoke").(*VarietySymbol); !ok {
+		return nil, span.Errorf(nil, "translator.Invoke must be a variety, got %v", arg.Walk("Invoke"))
+	}
+	if t.Combine, ok = arg.Walk("Combine").(*VarietySymbol); !ok {
+		return nil, span.Errorf(nil, "translator.Combine must be a variety, got %v", arg.Walk("Combine"))
+	}
+	return t, nil
+}
