43d84a0cbf24e8514cca8ed599d2da85740d3d5f
Petar Maymounkov <petarm@gmail.com>
2018-04-30 10:56:57 -0400

    renamings in go/gate

--- lang/go/gate/gate.go
+++ lang/go/gate/gate.go
@@ -36,14 +36,14 @@ func BindGate(receiver reflect.Type) (Gate, error) {
 // Gate represents a Go struct type with a Play method and Ko function bindings.
 type Gate struct {
 	Receiver reflect.Type `ko:"name=receiver"`
-	Struct   Struct       `ko:"name=arg"`
+	Struct   GateStruct   `ko:"name=arg"`
 }
 
 func (f Gate) GoPkgPath() string { return f.Struct.PkgPath() }
 
 func (f Gate) GoName() string { return f.Struct.Name() }
 
-func (f Gate) Arg() []Field { return f.Struct.Field() }
+func (f Gate) Arg() GateFields { return f.Struct.Field() }
 
 func (f Gate) Returns() reflect.Type {
 	play, ok := f.Receiver.MethodByName("Play")
@@ -54,37 +54,37 @@ func (f Gate) Returns() reflect.Type {
 }
 
 // BindStruct verifies that the given Go struct has a correctly-implemented Ko interface (i.e. bindings).
-func BindStruct(t reflect.Type) (Struct, error) {
+func BindStruct(t reflect.Type) (GateStruct, error) {
 	if t.Kind() != reflect.Struct {
-		return Struct{}, fmt.Errorf("implementation %s must be a go struct type", t.Name())
+		return GateStruct{}, fmt.Errorf("implementation %s must be a go struct type", t.Name())
 	}
 	// verify all fields are public with ko tags
 	koNames := map[string]bool{}
 	monadic := false
 	for i := 0; i < t.NumField(); i++ {
 		f := t.Field(i)
-		koName, ok := Field{f}.getFieldKoName()
+		koName, ok := GateField{f}.getFieldKoName()
 		if !ok {
-			return Struct{}, fmt.Errorf("field %s missing ko name tag", f.Name)
+			return GateStruct{}, fmt.Errorf("field %s missing ko name tag", f.Name)
 		}
 		if koNames[koName] {
-			return Struct{}, fmt.Errorf("duplicate field %s", f.Name)
+			return GateStruct{}, fmt.Errorf("duplicate field %s", f.Name)
 		}
 		koNames[koName] = true
-		if (Field{f}).IsMonadic() {
+		if (GateField{f}).IsMonadic() {
 			if monadic {
-				return Struct{}, fmt.Errorf("multiple monadic arguments")
+				return GateStruct{}, fmt.Errorf("multiple monadic arguments")
 			} else {
 				monadic = true
 			}
 		}
 	}
-	return Struct{t}, nil
+	return GateStruct{t}, nil
 }
 
 func MonadicStructField(s reflect.Type) (reflect.StructField, bool) {
 	for i := 0; i < s.NumField(); i++ {
-		f := Field{s.Field(i)}
+		f := GateField{s.Field(i)}
 		if f.IsMonadic() {
 			return f.StructField, true
 		}
@@ -92,17 +92,17 @@ func MonadicStructField(s reflect.Type) (reflect.StructField, bool) {
 	return reflect.StructField{}, false
 }
 
-// Struct represents a Go struct type with Ko bindings.
-type Struct struct{ reflect.Type }
+// GateStruct represents a Go struct type with Ko bindings.
+type GateStruct struct{ reflect.Type }
 
-func (s Struct) GoPkgPath() string { return s.Type.PkgPath() }
+func (s GateStruct) GoPkgPath() string { return s.Type.PkgPath() }
 
-func (s Struct) GoName() string { return s.Type.Name() }
+func (s GateStruct) GoName() string { return s.Type.Name() }
 
-func (s Struct) Field() []Field {
-	r := make([]Field, 0, s.Type.NumField())
+func (s GateStruct) Field() GateFields {
+	r := make(GateFields, 0, s.Type.NumField())
 	for i := 0; i < s.Type.NumField(); i++ {
-		f := Field{s.Type.Field(i)}
+		f := GateField{s.Type.Field(i)}
 		if f.IsVisibleInKo() {
 			r = append(r, f)
 		}
@@ -110,16 +110,16 @@ func (s Struct) Field() []Field {
 	return r
 }
 
-func (s Struct) FieldByKoName(name string) (Field, bool) {
+func (s GateStruct) FieldByKoName(name string) (GateField, bool) {
 	for _, f := range s.Field() {
 		if f.KoName() == name {
 			return f, true
 		}
 	}
-	return Field{}, false
+	return GateField{}, false
 }
 
-func (s Struct) String() string {
+func (s GateStruct) String() string {
 	var w bytes.Buffer
 	fmt.Fprintf(&w, "type %s struct {\n", s.GoName())
 	for _, a := range s.Field() {
@@ -129,32 +129,32 @@ func (s Struct) String() string {
 	return w.String()
 }
 
-// Field represents a Go struct field with Ko bindings.
-type Field struct{ reflect.StructField }
+// GateField represents a Go struct field with Ko bindings.
+type GateField struct{ reflect.StructField }
 
-func (f Field) IsGoExported() bool {
+func (f GateField) IsGoExported() bool {
 	n := f.StructField.Name
 	return len(n) > 0 && strings.ToUpper(n[:1]) == n[:1]
 }
 
-func (f Field) IsVisibleInKo() bool {
+func (f GateField) IsVisibleInKo() bool {
 	_, ok := f.getFieldKoName()
 	return ok
 }
 
-func (f Field) String() string {
+func (f GateField) String() string {
 	return fmt.Sprintf("%s %v `%s`", f.GoName(), f.StructField.Type, f.StructField.Tag)
 }
 
-func (f Field) Name() KoGoName {
+func (f GateField) Name() KoGoName {
 	return KoGoName{Ko: f.KoName(), Go: f.GoName()}
 }
 
-func (f Field) GoName() string {
+func (f GateField) GoName() string {
 	return f.StructField.Name
 }
 
-func (f Field) KoName() string {
+func (f GateField) KoName() string {
 	if name, ok := f.getFieldKoName(); ok {
 		return name
 	} else {
@@ -162,7 +162,7 @@ func (f Field) KoName() string {
 	}
 }
 
-func (f Field) IsOptional() bool {
+func (f GateField) IsOptional() bool {
 	return StructFieldIsOptional(f.StructField)
 }
 
@@ -184,7 +184,7 @@ func StructFieldIsOptional(toField reflect.StructField) bool {
 
 const Monadic = "monadic"
 
-func (f Field) IsMonadic() bool {
+func (f GateField) IsMonadic() bool {
 	return IsStructFieldMonadic(f.StructField)
 }
 
@@ -199,21 +199,21 @@ func IsStructFieldMonadic(sf reflect.StructField) bool {
 	return false
 }
 
-func (f Field) getFieldKoName() (string, bool) {
+func (f GateField) getFieldKoName() (string, bool) {
 	return StructFieldKoProtoGoName(f.StructField)
 }
 
-func StripFields(s reflect.Type) Fields {
-	r := make(Fields, s.NumField())
+func StripFields(s reflect.Type) GateFields {
+	r := make(GateFields, s.NumField())
 	for i := 0; i < s.NumField(); i++ {
-		r[i] = &Field{s.Field(i)}
+		r[i] = GateField{s.Field(i)}
 	}
 	return r
 }
 
-type Fields []*Field
+type GateFields []GateField
 
-func (fields Fields) Monadic() (int, bool) {
+func (fields GateFields) Monadic() (int, bool) {
 	for i, f := range fields {
 		if f.IsMonadic() {
 			return i, true
@@ -222,7 +222,7 @@ func (fields Fields) Monadic() (int, bool) {
 	return -1, false
 }
 
-func (fields Fields) FieldByKoName(name string) (int, bool) {
+func (fields GateFields) FieldByKoName(name string) (int, bool) {
 	for i, f := range fields {
 		if f.KoName() == name {
 			return i, true
