18bc62fc1e2a7ea0735ba668293245995b05fbd0
Petar Maymounkov <petarm@gmail.com>
2018-05-10 11:49:06 -0400

    lib: add integer permutation

---
+++ lib/integer/perm.go
@@ -0,0 +1,25 @@
+package integer
+
+import (
+	"math/rand"
+
+	. "github.com/kocircuit/kocircuit/lang/go/eval"
+	"github.com/kocircuit/kocircuit/lang/go/runtime"
+)
+
+func init() {
+	RegisterEvalGate(new(GoPermuteInt64))
+}
+
+type GoPermuteInt64 struct {
+	Len  int64 `ko:"name=len,monadic"`
+	Seed int64 `ko:"name=seed"`
+}
+
+func (g *GoPermuteInt64) Play(ctx *runtime.Context) []int64 {
+	r := make([]int64, g.Len)
+	for i, index := range rand.New(rand.NewSource(g.Seed)).Perm(int(g.Len)) {
+		r[i] = int64(index)
+	}
+	return r
+}
