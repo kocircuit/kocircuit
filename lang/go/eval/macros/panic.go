253ce7e711dd8ec3b4cad22e6c22c2cceb97eaec
Ewout Prangsma <ewout@prangsma.net>
2018-10-04 10:26:50 +0200

    Reverted deconstruction changes

--- lang/go/eval/macros/panic.go
+++ lang/go/eval/macros/panic.go
@@ -93,7 +93,7 @@ func (EvalRecoverMacro) Invoke(span *Span, arg Arg) (returns Return, effect Effe
 	}
 	defer func() {
 		if r := recover(); r != nil {
-			evalPanic, _ := r.(*EvalPanic)
+			evalPanic := r.(*EvalPanic)
 			panicFields := Fields{{Name: "", Shape: evalPanic.Panic}}
 			returns, effect, err = panicVty.Evoke(span, panicFields)
 			return
