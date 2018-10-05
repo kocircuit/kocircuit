6b726cc5d70787a6ccdfa0d8e533b6185e9512a9
Ewout Prangsma <ewout@prangsma.net>
2018-10-05 09:37:19 +0200

    os package improvements

--- lib/os/os.go
+++ lib/os/os.go
@@ -1,46 +1,76 @@
+//
+// Copyright © 2018 Aljabr, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 package os
 
 import (
 	"os"
 
-	. "github.com/kocircuit/kocircuit/lang/go/eval"
+	go_eval "github.com/kocircuit/kocircuit/lang/go/eval"
 	"github.com/kocircuit/kocircuit/lang/go/runtime"
 )
 
 func init() {
-	RegisterEvalGate(new(GoMkdir))
-	RegisterEvalGate(new(GoEnv))
-	RegisterEvalGate(new(GoTempDir))
-	RegisterEvalGate(new(GoExit))
+	go_eval.RegisterNamedEvalGate("Mkdir", new(goMkdir))
+	go_eval.RegisterNamedEvalGate("Env", new(goEnv))
+	go_eval.RegisterNamedEvalGate("TempDir", new(goTempDir))
+	go_eval.RegisterNamedEvalGate("Exit", new(goExit))
 }
 
-type GoMkdir struct {
+type goMkdir struct {
 	Path string `ko:"name=path,monadic"`
 }
 
-func (mkdir *GoMkdir) Play(ctx *runtime.Context) error {
+func (mkdir *goMkdir) Play(ctx *runtime.Context) error {
 	return os.MkdirAll(mkdir.Path, 0755)
 }
 
-type GoEnv struct {
+func (*goMkdir) Doc() string  { return "Mkdir(path?) creates the given directory and all its parents" }
+func (*goMkdir) Help() string { return "Mkdir(path?)" }
+
+type goEnv struct {
 	Name string `ko:"name=name,monadic"`
 }
 
-func (env *GoEnv) Play(ctx *runtime.Context) string {
+func (env *goEnv) Play(ctx *runtime.Context) string {
 	return os.Getenv(env.Name)
 }
 
-type GoTempDir struct{}
+func (*goEnv) Doc() string {
+	return "Env(name?) returns the value of the environment variable with given name"
+}
+func (*goEnv) Help() string { return "Env(name?)" }
 
-func (GoTempDir) Play(ctx *runtime.Context) string {
+type goTempDir struct{}
+
+func (goTempDir) Play(ctx *runtime.Context) string {
 	return os.TempDir()
 }
 
-type GoExit struct {
+func (*goTempDir) Doc() string  { return "TempDir() returns the full path of the temporary directory" }
+func (*goTempDir) Help() string { return "TempDir()" }
+
+type goExit struct {
 	ExitCode int `ko:"name=code,monadic"`
 }
 
-func (g *GoExit) Play(ctx *runtime.Context) error {
+func (g *goExit) Play(ctx *runtime.Context) error {
 	os.Exit(g.ExitCode)
 	return nil
 }
+
+func (*goExit) Doc() string  { return "Exit(code?) terminates the process with given exit code" }
+func (*goExit) Help() string { return "Exit(code?)" }
--- lib/os/shell.go
+++ lib/os/shell.go
@@ -1,26 +1,20 @@
+//
+// Copyright © 2018 Aljabr, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 package os
 
-import (
-	"fmt"
-
-	"github.com/kocircuit/kocircuit/lang/go/runtime"
-)
-
+// ShellBinary holds the path of the shell command
 const ShellBinary = "/bin/sh"
-
-type GoWipeDir struct {
-	Dir string `ko:"name=dir,monadic"`
-}
-
-func (wipe *GoWipeDir) Play(ctx *runtime.Context) bool {
-	return wipe.Task().Play(ctx)
-}
-
-func (wipe *GoWipeDir) Task(after ...*GoTask) *GoTask {
-	return &GoTask{
-		Name:   fmt.Sprintf("wipeDir: %s", wipe.Dir),
-		Binary: ShellBinary,
-		Arg:    []string{"-c", fmt.Sprintf("rm -rf %s/*", wipe.Dir)},
-		After:  after,
-	}
-}
